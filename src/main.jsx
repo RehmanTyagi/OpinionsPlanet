8809668c5ca8cd561443c92eb9e16a1bd66fa1c8
Rehman Tyagi <irontyagi4@gmail.com>
2023-10-13 23:03:53 +0530

    added all libs

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./main.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+// tech stacks
+
+// => react query firebase css-modules react-router-dom react hooks form redux
